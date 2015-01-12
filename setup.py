d9fb23c1be1fdbe86d84472b2a8919e6be4f1032
Eric Rasche <rasche.eric@yandex.ru>
2015-01-12 16:28:04 -0600

    I give up on reqs...

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
-requirements = [x.strip() for x in open('requirements.txt', 'r').readlines()]
 
 setup(name="gxargparse",
-        version='0.1',
+        version='0.1.1',
         description='Galaxy drop-in replacement for argparse',
         author='Eric Rasche',
         author_email='rasche.eric@yandex.ru',
         license='GPL3',
-        requires=requirements,
+        requires=['galaxyxml'],
         url='https://github.com/erasche/gxargparse',
         packages=["argparse"],
         classifiers=[
