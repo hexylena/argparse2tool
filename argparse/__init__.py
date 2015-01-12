118d2c0aefaeb1833355e0cca567714c6248ae72
Eric Rasche <rasche.eric@yandex.ru>
2015-01-12 11:13:08 -0600

    Rewrite for proper loading outside of package
    Some of the most technically interesting code I've written in a while.
    
    - We can load the module after finding it.
    - In order to find it, we need to know where the REAL stdlib is, so...
        - we search each individual path in sys.path for the query ('argparse')
        - If a module that can be loaded by the name of 'argparse' is found, then
          we check against the string 'gxargparse'. Because of how python packages
          are installed, we have a reasonable guaruntee that will be in the pathname
          and if it is, we can immediately discard it
        - Once we find the correct one in the stdlib path we load that.
    
    Currently there are some improvements here:
    
    - use `sysconfig`
        - Not sure this will work perfectly:
    
            >>> import sysconfig
            >>> print sysconfig.get_path('stdlib')
            /tmp/test/env/lib/python2.7
    
        whilst another test produced the following paths for argparse search:
    
            /tmp/test/env/local/lib/python2.7/site-packages/gxargparse-0.1-py2.7.egg/argparse ('', '', 5)
            /tmp/test/env/lib/python2.7/site-packages/gxargparse-0.1-py2.7.egg/argparse ('', '', 5)
            /usr/lib/python2.7/argparse.py ('.py', 'U', 1)
    
        which makes me believe that we should load based on the information in the final tuple
    - Exits poorly in basically every scenario
        - No argparse in system path? Whoops, we'll exit trying to load a None, and die
        - Maybe 'gxargparse' isn't in the system path to the egg? Will say "hey,
        that looks like argparse" and die recursively trying to load itself

--- argparse/__init__.py
+++ argparse/__init__.py
@@ -2,18 +2,57 @@ import sys
 
 # Per @jmchilton's suggestion, and this SO question:
 # http://stackoverflow.com/a/6032023
-def import_non_local(name, custom_name=None):
-    import imp, sys
-    custom_name = custom_name or name
-    f, pathname, desc = imp.find_module(name, sys.path[1:])
-    module = imp.load_module(custom_name, f, pathname, desc)
+def copy_in_standard_module_symbols(name, not_name, local_module):
+    import imp
+
+    print sys.modules
+    for i in range(0, 100):
+        random_name = 'random_name_%d' % (i,)
+        if random_name not in sys.modules:
+            break
+        else:
+            random_name = None
+    if random_name is None:
+        raise RuntimeError("Couldn't manufacture an unused module name.")
+
+    # The code here originally simply did sys.path[1:] which excluded anything
+    # in the CWD, however that failed to account for the case where the library
+    # was installed, and was thus further down in the path. As a beautiful
+    # result of stdlib packages being stored in a different location from user
+    # installed packages, we can simply search through the paths until we get
+    # the one with the REAL stdlib package we're after.
+    # NB: This code is unlikely to work for nonstdlib overrides.
+
+    # This will hold the correct sys.path for the REAL argparse
+    correct_sys_path = []
+    for path in sys.path:
+        try:
+            (f, pathname, desc) = imp.find_module(name, [path])
+            if not_name not in pathname:
+                correct_sys_path = pathname
+                handle.close()
+                break
+        except:
+            # Many sys.paths won't contain the module of interest
+            pass
+
+    module = imp.load_module(random_name, f, pathname, desc)
     f.close()
-    return module
-ap = import_non_local('argparse', 'std_argparse')
-#import argparse
+    # Importing then returning the module name wreaks havock, and seems to
+    # cause a recursive import issue. We resolve this by copying the symbols
+    # from that module into the local module
+    #methods = sys.modules[random_name].__dict__.keys()
+    #del sys.modules[random_name]
+    #for key in methods:
+        #fake_key = 'real_' + key
+        #if not hasattr(local_module, fake_key):
+            #setattr(local_module, fake_key, getattr(module, key))
+    return sys.modules[random_name]
 
-
-
+ap = copy_in_standard_module_symbols('argparse', 'gxargparse', sys.modules[copy_in_standard_module_symbols.__module__])
+#print dir(ap)
+#print dir()
+#sys.exit()
 import galaxyxml.tool as gxt
 import galaxyxml.tool.parameters as gxtp
 import argparse_translation as at
