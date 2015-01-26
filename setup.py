3a308d2cef0276e5d90dcf3add00a9a5508c50d0
Eric Rasche <rasche.eric@yandex.ru>
2015-01-26 13:25:51 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.argv[-1] == 'publish':
 
 
 setup(name="gxargparse",
-        version='0.1.9',
+        version='0.2.0',
         description='Galaxy drop-in replacement for argparse',
         author='Eric Rasche',
         author_email='rasche.eric@yandex.ru',
