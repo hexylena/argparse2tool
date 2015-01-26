ae52d2bfa5f688211973dbfdea3859ff10f4ae02
Eric Rasche <rasche.eric@yandex.ru>
2015-01-26 13:27:04 -0600

    Fix requirements

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ if sys.argv[-1] == 'publish':
 
 
 setup(name="gxargparse",
-        version='0.2.0',
+        version='0.2.1',
         description='Galaxy drop-in replacement for argparse',
         author='Eric Rasche',
         author_email='rasche.eric@yandex.ru',
-        requires=['galaxyxml'],
+        install_requires=['galaxyxml'],
         url='https://github.com/erasche/gxargparse',
         packages=["argparse", "gxargparse"],
         entry_points={
