c56c79915835e172169acd8358fbdb0b21d77303
Anton Khodak <anton.khodak@ukr.net>
2016-07-01 20:45:50 +0300

    Fix #11: add missing requirements, update package info

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-from setuptools import setup, find_packages
-import sys, os
+import os
+import sys
+
+from setuptools import setup
+
 import argparse
+
 if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist bdist_wheel upload; git push")
     sys.exit()
 
 setup(name="gxargparse",
         version=argparse.__version__,
-        description='Galaxy drop-in replacement for argparse',
-        author='Eric Rasche',
-        author_email='rasche.eric@yandex.ru',
-        install_requires=['galaxyxml'],
-        url='https://github.com/erasche/gxargparse',
-        packages=["argparse", "gxargparse", "test"],
+        description='Instrument for forming Galaxy XML and CWL tool descriptions from argparse arguments',
+        author='Eric Rasche, Anton Khodak',
+        author_email='rasche.eric@yandex.ru, anton.khodak@ukr.net',
+        install_requires=['galaxyxml', 'jinja2', 'future'],
+        url='https://github.com/common-workflow-language/gxargparse',
+        packages=["argparse", "gxargparse"],
         entry_points={
             'console_scripts': [
                     'gxargparse_check_path = gxargparse.check_path:main',
-                    # 'arg2cwl = gxargparse.arg2cwl:main'
                 ]
             },
         classifiers=[
