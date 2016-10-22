2546e50b0f6dcc5376343de22edaf897e4dc8292
Anton Khodak <anton.khodak@ukr.net>
2016-10-22 23:19:10 +0300

    Fix #16

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 # /usr/bin/python3
-import io
 import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.', prefix_chars='-+', epilog="here's some epilog text", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -13,9 +12,7 @@ parser.add_argument('--sum', '-s', dest='accumulate', action='store_const',
         const=sum, default=max, help='sum the integers (default: find the max)')
 
 parser.add_argument('--foo', nargs='?', help='foo help')
-parser.add_argument('--file', nargs='?', help='foo help', type=io.IOBase)
 parser.add_argument('--bar', nargs='*', default=[1, 2, 3], help='BAR!')
-#parser.add_argument('+f', action='store_const', const='43')
 parser.add_argument('--true', action='store_true', help='Store a true')
 parser.add_argument('--false', action='store_false', help='Store a false')
 parser.add_argument('--append', action='append', help='Append a value')
