7f191f85e7402016aa77619e0b0d446734895345
Eric Rasche <rasche.eric@gmail.com>
2016-11-16 21:50:58 +0000

    Prep for next release

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
-import os
-import sys
-
 from setuptools import setup
 
 from argparse2tool import __version__
-
-if sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist bdist_wheel upload; git push")
-    sys.exit()
 
 setup(name="argparse2tool",
         version=__version__,
         description='Instrument for forming Galaxy XML and CWL tool descriptions from argparse arguments',
         author='Eric Rasche, Anton Khodak',
-        author_email='esr@tamu.edu, anton.khodak@ukr.net',
+        author_email='esr@tamu.edu; anton.khodak@ukr.net',
         install_requires=['galaxyxml>=0.2.3', 'jinja2', 'future'],
         url='https://github.com/erasche/argparse2tool',
         packages=["argparse", "argparse2tool", "click", "argparse2tool.cmdline2gxml", "argparse2tool.cmdline2cwl"],
@@ -23,7 +16,7 @@ setup(name="argparse2tool",
                 ]
             },
         classifiers=[
-            'Development Status :: 3 - Alpha',
+            'Development Status :: 4 - Beta',
             'Operating System :: OS Independent',
             'Intended Audience :: Developers',
             'Environment :: Console',
