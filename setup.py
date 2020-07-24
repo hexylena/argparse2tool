bb4d37885bf95ad8d2b6dfebb65618c2f3987bb7
Matthias Bernt <m.bernt@ufz.de>
2020-07-24 15:53:05 +0200

    pin galaxyxml version
    to ensure tests are passing also when galaxyxml is updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="argparse2tool",
         description='Instrument for forming Galaxy XML and CWL tool descriptions from argparse arguments',
         author='Helena Rasche, Anton Khodak',
         author_email='hxr@hx42.org',
-        install_requires=['galaxyxml>=0.2.3', 'jinja2', 'future'],
+        install_requires=['galaxyxml==0.4.6', 'jinja2', 'future'],
         url='https://github.com/erasche/argparse2tool',
         packages=["argparse", "argparse2tool", "click", "argparse2tool.cmdline2gxml", "argparse2tool.cmdline2cwl"],
         entry_points={
