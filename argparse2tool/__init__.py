da0b8763667c3431fec2681e5239d0334cccd5e1
Dusan Figala <dusanfigala@protonmail.com>
2024-10-31 09:57:28 +0100

    Replace imp with importlib
    imp is removed in ^3.12

--- argparse2tool/__init__.py
+++ argparse2tool/__init__.py
@@ -1,22 +1,26 @@
 """Stub for argparse2tool"""
+
 import sys
+
 try:
     from builtins import range
 except Exception:
     pass
 
 
-__version__ = '0.4.9'
+__version__ = "0.4.9"
 
 
 def load_argparse():
     ARGPARSE_NUMBER = 1
-    return load_conflicting_package('argparse', 'argparse2tool/dropins', ARGPARSE_NUMBER)
+    return load_conflicting_package(
+        "argparse", "argparse2tool/dropins", ARGPARSE_NUMBER
+    )
 
 
 def load_click():
     CLICK_NUMBER = 5
-    return load_conflicting_package('click', 'argparse2tool/dropins', CLICK_NUMBER)
+    return load_conflicting_package("click", "argparse2tool/dropins", CLICK_NUMBER)
 
 
 def load_conflicting_package(name, not_name, module_number):
@@ -28,9 +32,11 @@ def load_conflicting_package(name, not_name, module_number):
 
      http://stackoverflow.com/a/6032023
     """
-    import imp
+    import importlib.util
+    import importlib.machinery
+
     for i in range(0, 100):
-        random_name = 'random_name_%d' % (i,)
+        random_name = f"random_name_{i}"
         if random_name not in sys.modules:
             break
         else:
@@ -41,10 +47,20 @@ def load_conflicting_package(name, not_name, module_number):
     # This will hold the correct sys.path for the REAL argparse
     for path in sys.path:
         try:
-            (f, pathname, desc) = imp.find_module(name, [path])
+            spec = importlib.machinery.PathFinder.find_spec(name, [path])
+            if spec is None or spec.origin is None:
+                continue
+
+            # Check if this is the module we want (avoiding the override)
+            if not_name not in spec.origin and isinstance(
+                spec.loader, importlib.machinery.SourceFileLoader
+            ):
+                spec.loader.name = random_name
+                spec.name = random_name
+                module = importlib.util.module_from_spec(spec)
+                sys.modules[random_name] = module
+                spec.loader.exec_module(module)
+                return sys.modules[random_name]
         except ImportError:
             continue
-        if not_name not in pathname and desc[2] == module_number:
-            imp.load_module(random_name, f, pathname, desc)
-            return sys.modules[random_name]
     return None
